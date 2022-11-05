4c4788d8bc83f1d63cef33c1d81b7009190d1916
Crocifisso Pusateri <crocifissovpusater@lewisu.edu>
2022-11-05 14:42:15 -0500

    updates

--- client/map-mania-two.js
+++ client/map-mania-two.js
@@ -1,5 +1,5 @@
 var gMap;
-var score = 0
+var updateScore = 0
 
 
 function initMap() {
@@ -12,8 +12,8 @@ function initMap() {
         var infoWindow1 = new google.maps.InfoWindow({content:'Tokyo, Japan'});
         loc1.addListener('click', function() {
             infoWindow1.open(gMap, loc1);
-            ++score
-            setScore(score)
+            ++updateScore
+            setScore(updateScore)
         });
 
     var loc2 = new google.maps.Marker({position:{lat:42.0829,lng:-85.8667}, map:gMap});
@@ -22,8 +22,8 @@ function initMap() {
         var infoWindow2 = new google.maps.InfoWindow({content:'Gravel Lake, Michigan'});
         loc2.addListener('click', function() {
             infoWindow2.open(gMap, loc2);
-            ++score
-            setScore(score)
+            ++updateScore
+            setScore(updateScore)
         });
 
     var loc3 = new google.maps.Marker({position:{lat:41.3505,lng:-88.8419}, map:gMap});
@@ -32,8 +32,8 @@ function initMap() {
         var infoWindow3 = new google.maps.InfoWindow({content:'Ottawa, Illinois'});
         loc3.addListener('click', function() {
             infoWindow3.open(gMap, loc3);
-            ++score
-            setScore(score)
+            ++updateScore
+            setScore(updateScore)
         });
     
     var loc4 = new google.maps.Marker({position:{lat:41.3204,lng:-88.9935}, map:gMap});
@@ -42,8 +42,8 @@ function initMap() {
         var infoWindow4 = new google.maps.InfoWindow({content:'Starved Rock State Park, Illinois'});
         loc4.addListener('click', function() {
             infoWindow4.open(gMap, loc4);
-            ++score
-            setScore(score)
+            ++updateScore
+            setScore(updateScore)
         });
 
     var loc5 = new google.maps.Marker({position:{lat:25.7634,lng:-80.1916}, map:gMap});
@@ -52,8 +52,8 @@ function initMap() {
         var infoWindow5 = new google.maps.InfoWindow({content:'Miami, Florida'});
         loc5.addListener('click', function() {
             infoWindow5.open(gMap, loc5);
-            ++score
-            setScore(score)
+            ++updateScore
+            setScore(updateScore)
         });
 
     var loc6 = new google.maps.Marker({position:{lat:46.1423,lng:-88.0940}, map:gMap});
@@ -62,8 +62,8 @@ function initMap() {
         var infoWindow6 = new google.maps.InfoWindow({content:'Channing, Michigan'});
         loc6.addListener('click', function() {
             infoWindow6.open(gMap, loc6);
-            ++score
-            setScore(score)
+            ++updateScore
+            setScore(updateScore)
         });
 
     var loc7 = new google.maps.Marker({position:{lat:43.6274,lng:-89.7605}, map:gMap});
@@ -72,8 +72,8 @@ function initMap() {
         var infoWindow7 = new google.maps.InfoWindow({content:'Wisconsin Dells, Wisconsin'});
         loc7.addListener('click', function() {
             infoWindow7.open(gMap, loc7);
-            ++score
-            setScore(score)
+            ++updateScore
+            setScore(updateScore)
         });
 
     var loc8 = new google.maps.Marker({position:{lat:30.2695,lng:-97.7441}, map:gMap});
@@ -82,8 +82,8 @@ function initMap() {
         var infoWindow8 = new google.maps.InfoWindow({content:'Austin, Texas'});
         loc8.addListener('click', function() {
             infoWindow8.open(gMap, loc8);
-            ++score
-            setScore(score)
+            ++updateScore
+            setScore(updateScore)
         });
 
     var loc9 = new google.maps.Marker({position:{lat:41.6055,lng:-88.0783}, map:gMap});
@@ -92,8 +92,8 @@ function initMap() {
         var infoWindow9 = new google.maps.InfoWindow({content:'Lewis University, Illinois'});
         loc9.addListener('click', function() {
             infoWindow9.open(gMap, loc9);
-            ++score
-            setScore(score)
+            ++updateScore
+            setScore(updateScore)
         });
     var loc10 = new google.maps.Marker({position:{lat:41.8758,lng:-87.6242}, map:gMap});
     loc10.setIcon('http://maps.google.com/mapfiles/kml/pushpin/blue-pushpin.png');
@@ -101,11 +101,11 @@ function initMap() {
         var infoWindow10 = new google.maps.InfoWindow({content:'Chicago, Illinois'});
         loc10.addListener('click', function() {
             infoWindow10.open(gMap, loc10);
-            ++score
-            setScore(score)
+            ++updateScore
+            setScore(updateScore)
         });
 
-        setScore(score);
+        setScore(updateScore);
         setHint("hint")
 
     google.maps.event.addListener(gMap,'idle', function() {
@@ -176,7 +176,7 @@ function cheat(){
     let text = "Are you sure you want to cheat?";
     if (confirm(text) == true){
         score + 10
-        setScore(score)
+        setScore(updateScore)
     } else {
         text = "You are not a cheater.";
     }
