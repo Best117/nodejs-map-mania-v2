e0b0e0173ac30398be781c63c053b38c7f4d7297
Crocifisso Pusateri <crocifissovpusater@lewisu.edu>
2022-11-04 19:59:48 -0500

    updates

--- client/map-mania-two.js
+++ client/map-mania-two.js
@@ -6,19 +6,21 @@ var modal = document.getElementById("infoModal");
 var btn = document.getElementById("button");
 
 var span = document.getElementsByClassName("close")[0];
- 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
 
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onload = function(){
+    btn.onclick = function() {
+        modal.style.display = "block";
+      }
+      
+      span.onclick = function() {
+        modal.style.display = "none";
+      }
+      
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
 }
 
 function initMap() {
